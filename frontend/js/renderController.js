0c4cfd444f8f70f1f7ba5ac9d5bab84515abac17
Hamed Shahidi <hamed.shahidi@tietoevry.com>
2025-03-29 02:44:21 +0200

    refactor(ui): move renderPrices logic to renderController.js
    - Extracts rendering logic into its own controller
    - appController.js now imports renderPrices from renderController
    - Prepares UI layer for future DOM rendering improvements
    
    Refs #38

---
+++ frontend/js/renderController.js
@@ -0,0 +1,10 @@
+/**
+ * Renders electricity price data to the UI.
+ * Currently logs the data, will be extended to update DOM.
+ *
+ * @param {Object} data - Contains `today` and `tomorrow` arrays
+ */
+export function renderPrices(data) {
+    console.log('⚡ Price Data:', data);
+    // TODO: Implement DOM updates here
+  }
